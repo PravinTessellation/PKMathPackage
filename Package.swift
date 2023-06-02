5ca4a18b069c26f996698f0decd64e90b670fcfe
Pravin <pkulkarni@tessellationdev.com>
2023-06-02 14:58:27 +0530

    added zip file practice

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 
+/*
 let package = Package(
     name: "MathPackage",
     products: [
@@ -17,10 +18,28 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
         .binaryTarget(name: "SimpleMath", path: "./Sources/SimpleMath.xcframework"),
         .binaryTarget(name: "Addition", path: "./Sources/Addition.xcframework"),
+    ]
+)
+*/
+
+let package = Package(
+    name: "MathPackage",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "SimpleMath",
+            targets: ["SimpleMath"]),
+        .library(
+            name: "Addition",
+            targets: ["Addition"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "Addition", url: "https://github.com/PravinTessellation/zipfiles/raw/main/SimplePackage_old/Addition.xcframework.zip", checksum: "cd128fd76d71bdf69c7d4317d83d36f415db99f6523fca01ff7ce8287f9e8097"),
+        .binaryTarget(name: "SimpleMath", url: "https://github.com/PravinTessellation/zipfiles/raw/main/SimplePackage_old/SimpleMath.xcframework.zip", checksum: "ac314275ede25458964684a296aff24a867902d6e42c747bcb085e059138dfa6"),
     ]
 )
